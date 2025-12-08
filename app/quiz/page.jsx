7cce0c19ef76bd90afb2d06678f71f644b27f261
RaselMridha792 <raselmridha792@gmail.com>
2025-12-08 22:44:33 +0600

    setup comming soon page

---
+++ app/quiz/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='min-h-lvh text-center flex items-center justify-center bg-black text-white text-7xl'>quiz page</div>
+  )
+}
