3f49b6f0ad97507de949b942fee35a6eb61e36ed
RaselMridha792 <raselmridha792@gmail.com>
2025-12-09 10:22:01 +0600

    testing

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='min-h-lvh text-center flex items-center justify-center bg-black text-white text-7xl'>quiz page</div>
+    <div className='min-h-lvh text-center flex items-center justify-center bg-black text-white text-7xl'>quiz page hlw
+    </div>
   )
 }
