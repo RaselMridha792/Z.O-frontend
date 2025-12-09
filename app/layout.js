4a3486b3a48f442837084860de0e0e91cb80cb1e
tumit-h-r-75 <tumithasan1@gmail.com>
2025-12-09 16:21:11 +0600

    move the hero section on the home file

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import HeroSection from "./Components/HeroSection";
+import HeroSection from "./Components/Home/HeroSection";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
