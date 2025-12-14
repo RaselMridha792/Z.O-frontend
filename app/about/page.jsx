8728f1281b3cc273f618a368098ac3ed3064ed4c
unknown <shoriful.dev95@gmail.com>
2025-12-14 18:49:17 +0600

    add edit button profile page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -384,7 +384,6 @@ const Section = () => {
               height="315"
               src="https://www.youtube.com/embed/I84Du2wNqHc?si=DsvuibtYlQmZkEuP"
               title="YouTube video player"
-              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
               allowFullScreen
--- app/dashboard/profile/page.jsx
+++ app/dashboard/profile/page.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-
+import { FaRegEdit } from "react-icons/fa";
 const page = () => {
   return (
     <main className="border-2 border-red-500 p-5 shadow-lg rounded-xl ">
       <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md transition-all ease-in-out duration-300 flex flex-col gap-5">
-        <h1 className="text-2xl lg:text-3xl font-semibold lg:mb-6">
-          My Profile
-        </h1>
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl lg:text-3xl font-semibold lg:mb-6">
+            My Profile
+          </h1>
+          <button className="cursor-pointer flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+            <FaRegEdit />
+
+            <span className="mx-1">Edit</span>
+          </button>
+        </div>
+
         <div className=" border-red-300 flex justify-start text-lg lg:text-xl">
           <h2 className="w-1/2 font-semibold">Registration Date</h2>
           <p>February 25, 2025 6:01 am</p>
